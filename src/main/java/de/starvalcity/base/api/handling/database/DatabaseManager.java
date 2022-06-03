606f5dba6e931290264cfde5a4d5f294981f8116
Nirflector <nirflector@gmail.com>
2022-06-03 13:09:09 +0200

    Edit SQL functions

--- src/main/java/de/starvalcity/base/api/handling/database/DatabaseManager.java
+++ src/main/java/de/starvalcity/base/api/handling/database/DatabaseManager.java
@@ -1,5 +1,9 @@
 package de.starvalcity.base.api.handling.database;
 
+import de.starvalcity.base.Core;
+import de.starvalcity.base.background.def.CustomizedFile;
+import org.bukkit.scheduler.BukkitRunnable;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,6 +17,8 @@ public class DatabaseManager {
     private final String password;
     private final String database;
     private final int port;
+
+    private final CustomizedFile databaseConfiguration = new CustomizedFile("plugins//Liquid//Configuration", "DatabaseConfiguration.yml");
 
     public DatabaseManager(String host, String username, String password, String database, int port) {
         this.host = host;
@@ -29,6 +35,7 @@ public class DatabaseManager {
                     ":" + port +
                     "/" + database +
                     "?useUnicode=true&characterEncoding=utf-8&autoReconnect=true", username, password);
+            createDefaultFiles();
         } catch (ClassNotFoundException | SQLException classNotFoundException) {
             classNotFoundException.printStackTrace();
         }
@@ -69,6 +76,91 @@ public class DatabaseManager {
     }
 
     public void insert(String query) {
+        if (databaseConfiguration.getObject("Use_Threads").equals(true)) {
+            executeAsynchronously(query, "INSERT");
+        } else {
+            try {
+                getConnection().createStatement().executeUpdate(query);
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
+    }
 
+    public void update(String query) {
+        if (databaseConfiguration.getObject("Use_Threads").equals(true)) {
+            executeAsynchronously(query, "UPDATE");
+        } else {
+            try {
+                getConnection().createStatement().executeUpdate(query);
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
+    }
+
+    public void delete(String query) {
+        if (databaseConfiguration.getObject("Use_Threads").equals(true)) {
+            executeAsynchronously(query, "DELETE");
+        } else {
+            try {
+                getConnection().createStatement().executeUpdate(query);
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
+    }
+
+    public boolean execute(String query) {
+        try {
+            getConnection().createStatement().execute(query);
+            return true;
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean tableExists(String table) {
+        try {
+            ResultSet tables = getConnection().getMetaData().getTables(null, null, table, null);
+            return tables.next();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean columnExists(String table, String column) {
+        try {
+            ResultSet columns = getConnection().getMetaData().getColumns(null, null, table, column);
+            return columns.next();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            return false;
+        }
+    }
+
+    private void executeAsynchronously(String query, String sqlType) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    if (getConnection() != null) {
+                        getConnection().createStatement().executeUpdate(query);
+                    }
+                } catch (SQLException sqlException) {
+                    sqlException.printStackTrace();
+                }
+            }
+        }.runTaskAsynchronously(Core.getPlugin());
+    }
+
+    private void createDefaultFiles() {
+        if (!databaseConfiguration.exist()) {
+            databaseConfiguration.setDefaultValue("Use_Threads", true);
+            databaseConfiguration.save();
+        }
+        databaseConfiguration.save();
     }
 }
